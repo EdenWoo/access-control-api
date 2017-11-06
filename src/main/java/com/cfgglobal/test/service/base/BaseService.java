7a206f19b1020f3afe12c8ecb2b4bb89a5fb589c
leonzhou <lei.zhou@cfgglobal.co.nz>
2017-11-06 13:40:11 +1300

    base service

--- src/main/java/com/cfgglobal/test/service/base/BaseService.java
+++ src/main/java/com/cfgglobal/test/service/base/BaseService.java
@@ -75,10 +75,10 @@ public abstract class BaseService<T, ID extends Serializable> {
         return baseDao.findByFilter(queryFilters.appendAll(securityFilters));
     }
 
-    public T findOneBySecurity(Long id, String method, String requestURI) {
+    public T findOneBySecurity(ID id, String method, String requestURI) {
         List<Filter> securityFilters = securityFilter.query(method, requestURI);
         List<T> list = baseDao.findByFilter(securityFilters);
-        T entity = baseDao.findById((ID) id).get();
+        T entity = baseDao.findById(id).get();
         if (list.contains(entity)) {
             return entity;
         } else {
