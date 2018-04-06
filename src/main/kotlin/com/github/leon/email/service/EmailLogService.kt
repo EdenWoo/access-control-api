39241d6ca0bcdd178d79a5cd1fc00d7f8dc3411f
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-04-06 16:53:35 +1200

    email

--- src/main/kotlin/com/github/leon/email/service/EmailLogService.kt
+++ src/main/kotlin/com/github/leon/email/service/EmailLogService.kt
@@ -1,21 +1,27 @@
 package com.github.leon.email.service
 
-import com.github.leon.email.dao.EmailLogDao
-import com.github.leon.email.domain.EmailLog
+import com.github.leon.aci.enums.TaskStatus
 import com.github.leon.aci.service.base.BaseService
 import com.github.leon.aci.vo.Condition
 import com.github.leon.aci.vo.Filter
+import com.github.leon.email.dao.EmailLogDao
+import com.github.leon.email.domain.EmailLog
+import com.github.leon.freemarker.FreemarkerBuilderUtil
 import org.apache.commons.lang3.math.NumberUtils
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.nio.charset.Charset
 
 @Service
-class EmailLogService : BaseService<EmailLog, Long>() {
+class EmailLogService(
+        @Autowired
+        val emailLogDao: EmailLogDao,
+        @Autowired
+        val freemarkerBuilderUtil: FreemarkerBuilderUtil
+) : BaseService<EmailLog, Long>() {
 
-    @Autowired
-    private val emailLogDao: EmailLogDao? = null
-
-
+    val log = LoggerFactory.getLogger(EmailLogService::class.java)!!
     fun findForSend(customEmail: EmailLog): List<EmailLog> {
         val cond1 = Condition(
                 fieldName = "status",
@@ -37,14 +43,28 @@ class EmailLogService : BaseService<EmailLog, Long>() {
         return findByFilter(filter)
     }
 
+    fun sendSystem(subject: String, sendTo: String, ftl: String, model: Map<String, Any?>) {
+        try {
+            val emailLog = EmailLog(
+                    times = 0,
+                    sendTo = sendTo,
+                    subject = subject,
+                    content = freemarkerBuilderUtil.build(ftl, model)!!.toByteArray(Charset.forName("UTF-8")),
+                    status = TaskStatus.TODO)
+            emailLogDao.save(emailLog)
+        } catch (e: Exception) {
+            log.error("email send error", e)
+        }
+    }
+
 
     fun update(customEmailLog: EmailLog) {
-        emailLogDao!!.save(customEmailLog)
+        emailLogDao.save(customEmailLog)
     }
 
 
     operator fun get(id: String): EmailLog {
-        return emailLogDao!!.findOne(NumberUtils.createLong(id)!!)
+        return emailLogDao.findOne(NumberUtils.createLong(id)!!)
     }
 
     operator fun get(id: Long?): EmailLog {
