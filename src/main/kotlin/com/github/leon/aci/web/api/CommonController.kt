19a6f4f9d08c6d92723f842b96ddd2813cf31be7
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-04-06 15:37:53 +1200

    add excel

--- src/main/kotlin/com/github/leon/aci/web/api/CommonController.kt
+++ src/main/kotlin/com/github/leon/aci/web/api/CommonController.kt
@@ -13,22 +13,34 @@ import com.github.leon.aci.service.base.BaseService
 import com.github.leon.aci.vo.Condition
 import com.github.leon.aci.vo.Filter
 import com.github.leon.aci.web.base.BaseController
+import com.github.leon.ams.s3.AmazonService
+import com.github.leon.ams.s3.UploadUtil
+import com.github.leon.extentions.execCmd
 import com.github.leon.extentions.remainLastIndexOf
 import org.joor.Reflect
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.io.File
+import java.io.OutputStreamWriter
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.Instant
 
 @RestController
 @RequestMapping("/v1")
 class CommonController(
         @Autowired
-        val context: ApplicationContext
+        val context: ApplicationContext,
+        @Autowired
+        val uploadUtil: UploadUtil,
+        @Autowired
+        val amazonService: AmazonService
 ) : BaseController() {
 
     @RequestMapping(value = ["/{entity}/{f}/{v}"], method = [RequestMethod.HEAD])
-    fun existenceCheck(@PathVariable entity: String, @PathVariable(required = false) f: String, @PathVariable v: String): ResponseEntity<String> {
+    fun existenceCheck(@PathVariable entity: String, @PathVariable f: String, @PathVariable v: String, id: Long?): ResponseEntity<String> {
         val service = context.getBean("${entity}Service") as BaseService<*, *>
         return Option.monad().binding {
             val field = f.toOption().bind()
@@ -38,6 +50,10 @@ class CommonController(
                     value = value,
                     operator = Filter.OPERATOR_EQ
             )))
+            id.toOption().forEach {
+                filter.conditions = filter.conditions + Condition(fieldName = "id", value = id, operator = Filter.OPERATOR_NOT_EQ)
+            }
+
             val result = Try { service.findByFilter(filter) }.toOption().bind()
             val status = when (result.size) {
                 0 -> 200
@@ -57,5 +73,34 @@ class CommonController(
                 .map { it.toString().remainLastIndexOf(".") }
                 .toList()
         return ResponseEntity.ok(value)
+    }
+
+
+    @GetMapping("/dump")
+    fun dump(): ResponseEntity<String> {
+        val name = "/tmp/${Instant.now().epochSecond}.sql"
+        val command = "mysqldump -uroot -proot collinson  role  permission rule role_permission role_permission_rule"
+        val result = command.execCmd()
+        Files.write(Paths.get(name), result)
+        val awsFileName = "/v1/attachment/download?filename=${uploadUtil.write(File(name), "")}"
+        return ResponseEntity.ok(awsFileName)
+    }
+
+
+    @GetMapping("/import")
+    fun importSql(filename: String) {
+        val file = amazonService.getFile(filename)
+        val command = "mysql -uroot -proot"
+        val command2 = "use collinson"
+        val command3 = "source ${file.name}"
+        val runtime = Runtime.getRuntime()
+
+        val process = runtime.exec(command)
+        val os = process.outputStream
+        val writer = OutputStreamWriter(os)
+        writer.write(command2 + "\r\n" + command3)
+        writer.flush()
+        writer.close()
+        os.close()
     }
 }
---
+++ src/main/kotlin/com/github/leon/excel/ExcelController.kt
@@ -0,0 +1,31 @@
+package com.github.leon.excel
+
+import com.github.leon.excel.service.ExcelParsingRule
+import com.github.leon.aci.web.base.BaseController
+import com.github.leon.files.PoiImporter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import java.time.Instant
+
+@RestController
+@RequestMapping("/v1/excel")
+
+class ExcelController : BaseController() {
+
+    @Autowired
+    var excelParsingRules: MutableList<ExcelParsingRule<*>> = mutableListOf()
+
+    @PostMapping
+    fun submit(file: MultipartFile, rule: String) {
+        val excelParsingRule = excelParsingRules.first { it.ruleName == rule }
+        val fileName = "/tmp/" + Instant.now().epochSecond
+        val tmpFile = File(fileName)
+        file.transferTo(tmpFile)
+        val fileParser = excelParsingRule.fileParser
+        excelParsingRule.process(PoiImporter.processSheet(tmpFile, fileParser, excelParsingRule.entityClass))
+    }
+}
