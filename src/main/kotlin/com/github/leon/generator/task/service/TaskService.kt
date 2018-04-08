36c51764ca9cca4ca3fbea0cd7553f92ada3e6c3
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-04-09 11:10:58 +1200

    replace file

--- src/main/kotlin/com/github/leon/generator/task/service/TaskService.kt
+++ src/main/kotlin/com/github/leon/generator/task/service/TaskService.kt
@@ -55,7 +55,7 @@ object TaskService {
         val filename = codeProject.scriptHelper.exec<Any>(task.filename, root).toString()
         val outputFilename = folder + File.separator + filename
         val outputFile = File(outputFilename)
-        if (task.replaceFile && !outputFile.exists()) {
+        if (task.replaceFile || !outputFile.exists()) {
             codeProject.templateEngine.exec(templateFilename, outputFilename)
         }
         return outputFilename
