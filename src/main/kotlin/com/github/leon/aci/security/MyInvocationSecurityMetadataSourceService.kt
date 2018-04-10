5aef992da429353a5ddc374e6255f2b070e2dd08
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-04-10 17:12:42 +1200

    compile error fix

--- src/main/kotlin/com/github/leon/aci/security/MyInvocationSecurityMetadataSourceService.kt
+++ src/main/kotlin/com/github/leon/aci/security/MyInvocationSecurityMetadataSourceService.kt
@@ -33,7 +33,7 @@ class MyInvocationSecurityMetadataSourceService(
     //此方法是为了判定用户请求的url 是否在权限表中，如果在权限表中，则返回给 decide 方法，用来判定用户是否有此权限。如果不在权限表中则放行。
     override fun getAttributes(`object`: Any): List<ConfigAttribute>? {
         val request = (`object` as FilterInvocation).httpRequest
-        val permissionOpt = cacheClient["permissions", { permissionDao.findAll() }]!!
+        val permissionOpt = cacheClient.get("permissions") { permissionDao.findAll() }!!
                 .firstOption { (_, _, _, authUris) ->
                     authUris.split(";").any { uriPatten -> Pattern.matches(uriPatten, request.requestURI) }
                 }
