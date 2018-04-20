5490d0654d3e05036bfb8e1dddacc7b6cfcb01dc
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-04-20 18:02:40 +1200

    math

--- src/main/kotlin/com/github/leon/math/MathUtil.kt
+++ src/main/kotlin/com/github/leon/math/MathUtil.kt
@@ -10,9 +10,9 @@ object MathUtil {
      * @param d 要操作的double数字
      * @return 两位小数的double。是向上取整 的。
      */
-    fun upToTwoDecimal(d: Double): Double? {
+    fun upToTwoDecimal(d: Double): Double {
         return if (d < 0) {
-            (-1 * upToDecimal(Math.abs(d), 2)!!)
+            (-1 * upToDecimal(Math.abs(d), 2))
         } else {
             upToDecimal(d, 2)
         }
@@ -25,24 +25,24 @@ object MathUtil {
      * @param decimalLen 位数
      * @return
      */
-    fun upToDecimal(d: Double?, decimalLen: Int): Double? {
+    fun upToDecimal(d: Double?, decimalLen: Int): Double {
         val bg = BigDecimal(d!!.toString() + "")
         return bg.setScale(decimalLen, BigDecimal.ROUND_CEILING).toDouble()
     }
 
-    fun halfUpToDecimal(d: Double?, decimalLen: Int): Double? {
+    fun halfUpToDecimal(d: Double?, decimalLen: Int): Double {
         val bg = BigDecimal(d!!.toString() + "")
         return halfUpToDecimal(bg, decimalLen)
     }
 
-    fun halfUpToDecimal(bg: BigDecimal, decimalLen: Int): Double? {
+    fun halfUpToDecimal(bg: BigDecimal, decimalLen: Int): Double {
         var bg = bg
         val plainString = bg.toPlainString()
         bg = BigDecimal(plainString)
         return bg.setScale(decimalLen, BigDecimal.ROUND_HALF_UP).toDouble()
     }
 
-    fun downToDeimal(d: Double?, decimalLen: Int): Double? {
+    fun downToDeimal(d: Double?, decimalLen: Int): Double {
         var bg = BigDecimal(d!!.toString() + "")
         val plainString = bg.toPlainString()
         bg = BigDecimal(plainString)
@@ -57,22 +57,16 @@ object MathUtil {
      * @param operator 操作符。+\-\*\/分别对应加减乘除
      * @return
      */
-    fun doubleOperate(a: Double, b: Double, operator: String): Double? {
+    fun doubleOperate(a: Double, b: Double, operator: String): Double {
         val aa = BigDecimal(a.toString())
         val bb = BigDecimal(b.toString())
-        var `val` = 0.0
-        if (operator == "/") {
-            `val` = aa.divide(bb, 10, BigDecimal.ROUND_HALF_UP).toDouble()
-        } else if (operator == "+") {
-            `val` = aa.add(bb).toDouble()
-        } else if (operator == "-") {
-            `val` = aa.subtract(bb).toDouble()
-        } else if (operator == "*") {
-            `val` = aa.multiply(bb).toDouble()
-        } else {
-            `val` = a
+        return when (operator) {
+            "/" -> aa.divide(bb, 10, BigDecimal.ROUND_HALF_UP).toDouble()
+            "+" -> aa.add(bb).toDouble()
+            "-" -> aa.subtract(bb).toDouble()
+            "*" -> aa.multiply(bb).toDouble()
+            else -> a
         }
-        return `val`
     }
 
     /**
@@ -83,33 +77,23 @@ object MathUtil {
      * @param operator 操作符。+\-\*\/分别对应加减乘除
      * @return
      */
-    fun doubleOperate(aa: BigDecimal, bb: BigDecimal, operator: String): BigDecimal? {
-        var `val`: BigDecimal? = null
-        if (operator == "/") {
-            `val` = aa.divide(bb, 10, BigDecimal.ROUND_HALF_UP)
-        } else if (operator == "+") {
-            `val` = aa.add(bb)
-        } else if (operator == "-") {
-            `val` = aa.subtract(bb)
-        } else if (operator == "*") {
-            `val` = aa.multiply(bb)
-        } else {
-            `val` = aa
+    fun doubleOperate(aa: BigDecimal, bb: BigDecimal, operator: String): BigDecimal {
+        return when (operator) {
+            "/" -> aa.divide(bb, 10, BigDecimal.ROUND_HALF_UP)
+            "+" -> aa.add(bb)
+            "-" -> aa.subtract(bb)
+            "*" -> aa.multiply(bb)
+            else -> aa
         }
-        return `val`
     }
 
     fun doubleOperateString(a: String): Int {
-        var `val` = 0
-        if (a.length - a.indexOf(".") == 3) {
-            `val` = Integer.valueOf(a.replace(".", ""))//应付金额Excel
-        } else if (a.indexOf(".") == -1) {
-            `val` = Integer.valueOf(a + "00")//没有小数，直接拼上00
-        } else {
-            `val` = Integer.valueOf(a.replace(".", "") + "0")//应收金额Excel
-        }
 
-        return `val`
+        return when {
+            a.length - a.indexOf(".") == 3 -> Integer.valueOf(a.replace(".", ""))//应付金额Excel
+            a.indexOf(".") == -1 -> Integer.valueOf(a + "00")//没有小数，直接拼上00
+            else -> Integer.valueOf(a.replace(".", "") + "0")//应收金额Excel
+        }
     }
 
 }
