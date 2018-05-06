797ba8126a61f6979e2014b042ae77e42942b83f
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-05-07 11:24:48 +1200

    findpassword

--- src/main/kotlin/com/github/leon/aci/dao/FindPwdSendLogDao.java
+++
@@ -1,9 +0,0 @@
-package com.github.leon.aci.dao;
-
-
-import com.github.leon.aci.dao.base.BaseDao;
-import com.github.leon.aci.domain.FindPwdSendLog;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FindPwdSendLogDao extends BaseDao<FindPwdSendLog, Long> {}
