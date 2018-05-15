2d1f4628f6d421a233e6466655ff38f3ebeb9d45
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-05-15 15:40:11 +1200

    excel

--- src/main/kotlin/com/github/leon/generator/metadata/ExcelFeature.kt
+++ src/main/kotlin/com/github/leon/generator/metadata/ExcelFeature.kt
@@ -2,6 +2,11 @@ package com.github.leon.generator.metadata
 
 @Target(AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class ExcelFeature(val importable:Boolean = false, val exportable:Boolean= false,  val column: String = "", val header: String= "")
+annotation class ExcelFeature(
+        val importable: Boolean = false,
+        val exportable: Boolean = false,
+        val column: String = "",
+        val header: String = ""
+)
 
 
