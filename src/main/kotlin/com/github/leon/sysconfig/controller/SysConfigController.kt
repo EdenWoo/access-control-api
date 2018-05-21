40708602d22ab18f50aafac33c5d2834f2d1f8f2
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-05-21 22:49:05 +1200

    sys config

--- src/main/kotlin/com/github/leon/sysconfig/controller/SysConfigController.kt
+++ src/main/kotlin/com/github/leon/sysconfig/controller/SysConfigController.kt
@@ -19,7 +19,7 @@ class SysConfigController(
         val sysConfigService: SysConfigService,
         @Autowired
         val userService: UserService
-) : BaseController<SysConfig,Long>() {
+) : BaseController<SysConfig, Long>() {
 
     @GetMapping("/can-trade")
     fun canTrade(): ResponseEntity<Map<*, *>> {
@@ -32,23 +32,24 @@ class SysConfigController(
     override fun page(pageable: Pageable, request: HttpServletRequest): ResponseEntity<Page<SysConfig>> {
         return super.page(pageable, request)
     }
+
     @GetMapping("{id}")
-    override fun findOne(id: Long, request: HttpServletRequest): ResponseEntity<SysConfig> {
+    override fun findOne(@PathVariable id: Long, request: HttpServletRequest): ResponseEntity<SysConfig> {
         return super.findOne(id, request)
     }
 
     @PostMapping
-    override fun saveOne(input: SysConfig, request: HttpServletRequest): ResponseEntity<*> {
+    override fun saveOne(@RequestBody input: SysConfig, request: HttpServletRequest): ResponseEntity<*> {
         return super.saveOne(input, request)
     }
 
     @PutMapping
-    override fun updateOne(id: Long, input: SysConfig, request: HttpServletRequest): ResponseEntity<*> {
+    override fun updateOne(@PathVariable id: Long, @RequestBody input: SysConfig, request: HttpServletRequest): ResponseEntity<*> {
         return super.updateOne(id, input, request)
     }
 
     @DeleteMapping("{id}")
-    override fun deleteOne(id: Long, request: HttpServletRequest): ResponseEntity<*> {
+    override fun deleteOne(@PathVariable id: Long, request: HttpServletRequest): ResponseEntity<*> {
         return super.deleteOne(id, request)
     }
 }
