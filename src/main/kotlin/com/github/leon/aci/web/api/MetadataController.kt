da85659f894cfae17113cc44dabf135b45d725d6
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-05-27 14:16:12 +1200

    task

--- src/main/kotlin/com/github/leon/aci/web/api/MetadataController.kt
+++ src/main/kotlin/com/github/leon/aci/web/api/MetadataController.kt
@@ -1,28 +1,23 @@
 package com.github.leon.aci.web.api
 
-import arrow.core.None
-import arrow.core.Some
-import arrow.core.toOption
 import com.github.leon.aci.dao.PermissionDao
 import com.github.leon.aci.dao.RoleDao
 import com.github.leon.aci.domain.BaseEntity
-import com.github.leon.aci.domain.Permission
-import com.github.leon.aci.domain.Role
+import com.github.leon.aci.extenstions.responseEntityOk
 import com.github.leon.aci.security.ApplicationProperties
 import com.github.leon.aci.service.GeneratorService
 import com.github.leon.aci.service.PermissionService
 import com.github.leon.aci.service.RoleService
-import com.github.leon.classpath.ClassSearcher
+import com.github.leon.generator.entity.Task
+import com.github.leon.generator.entity.TaskOfProject
 import org.joor.Reflect
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory
 import org.springframework.http.ResponseEntity
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.util.ClassUtils
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -53,12 +48,23 @@ class MetadataController(
     }
 
     @GetMapping("/entity")
-    fun entity(): ResponseEntity<List<Map<String,Any?>>> {
-        val list = ApplicationProperties.entityScanPackages.map { it.replace(".","/")+"/*.class" }
+    fun entity(): ResponseEntity<List<Map<String, Any?>>> {
+        val list = ApplicationProperties.entityScanPackages.map { it.replace(".", "/") + "/*.class" }
                 .flatMap {
                     findClasses(BaseEntity::class.java, it)
-                }.map { mutableMapOf("name" to it.name)}
+                }.map { mutableMapOf("name" to it.name) }
         return ResponseEntity.ok(list)
+
+    }
+
+    @GetMapping("/task")
+    fun task(): ResponseEntity<List<Task>> {
+        Reflect.on("com.rapiddev.generator.TaskConstants").call("init")
+        val list = findClasses(Task::class.java, "classpath*:com/rapiddev/generator/task/*/*.class")
+                .map {
+                    (it.newInstance() as Task)
+                }
+        return list.responseEntityOk()
 
     }
 
