bfb156ea5ca52e311c3d26cb47d916c0aa8ffdbc
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-05-29 15:20:06 +1200

    jackson filter

--- src/main/kotlin/com/github/leon/aci/config/json/JacksonJsonFilter.kt
+++ src/main/kotlin/com/github/leon/aci/config/json/JacksonJsonFilter.kt
@@ -15,11 +15,13 @@ import org.apache.commons.lang3.StringUtils
 import org.hibernate.collection.spi.PersistentCollection
 import org.hibernate.proxy.HibernateProxy
 import org.joor.Reflect
+import org.slf4j.LoggerFactory
 
 
 @JsonFilter("JacksonFilter")
 class JacksonJsonFilter : FilterProvider() {
 
+    val log = LoggerFactory.getLogger(JacksonJsonFilter::class.java)!!
     private val creatorShow = ThreadLocal.withInitial<MutableMap<String, Boolean>> { mutableMapOf() }
 
     internal var includeMap: MutableMap<Class<*>, MutableSet<String>> = mutableMapOf()
@@ -87,14 +89,15 @@ class JacksonJsonFilter : FilterProvider() {
         } else if (!includeFields.isEmpty() && !filterFields.isEmpty()) {
             //return true;
         }
-        if (type == User::class.java && name == "creator") {
+        if (flag && type == User::class.java && name == "creator") {
             val field = (type.declaredFields + type.superclass.declaredFields).first { it.name == name }
             val map = creatorShow.get()
             val id = Reflect.on(pojo).get<Long>("id")
             val creator: User = Reflect.on(pojo).get("creator")
             val creatorId = creator.id
             val rootRender = map.getOrDefault(id.toString() + field.name, false)
-            // val embeddedRender = map.getOrDefault(creatorId.toString() + field.name, false)
+            val embeddedRender = map.getOrDefault(creatorId.toString() + field.name, false)
+            log.debug("rootRender {} embeddedRender {}", rootRender, embeddedRender)
             if (rootRender) {
                 flag = false
             } else {
