206ce3f1f765ff09e045c6719ec670b445d59373
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-05-29 17:47:09 +1200

    metadata

--- src/main/kotlin/com/github/leon/aci/extenstions/AnyExtension.kt
+++ src/main/kotlin/com/github/leon/aci/extenstions/AnyExtension.kt
@@ -1,27 +1,13 @@
 package com.github.leon.aci.extenstions
 
-import arrow.core.getOrElse
-import arrow.core.toOption
 import org.springframework.http.ResponseEntity
 
-fun Any?.println() {
-    println(this)
+fun <T> T?.responseEntityOk(): ResponseEntity<T> {
+    return ResponseEntity.ok(this)
 }
 
-fun Any?.print() {
-    print(this)
-}
-
-fun <T> T?.orElse(default: T): T {
-    return this.toOption().getOrElse { default }
-}
-
-fun <T> T?.responseEntityOk():ResponseEntity<T> {
-    return  ResponseEntity.ok(this)
-}
-
-fun <T> T?.responseEntityBadRequest():ResponseEntity<T> {
-    return  ResponseEntity.badRequest().body(this)
+fun <T> T?.responseEntityBadRequest(): ResponseEntity<T> {
+    return ResponseEntity.badRequest().body(this)
 }
 
 
--- src/main/kotlin/com/github/leon/aci/web/api/MetadataController.kt
+++ src/main/kotlin/com/github/leon/aci/web/api/MetadataController.kt
@@ -1,19 +1,15 @@
 package com.github.leon.aci.web.api
 
-import com.github.leon.aci.dao.PermissionDao
-import com.github.leon.aci.dao.RoleDao
 import com.github.leon.aci.domain.BaseEntity
+import com.github.leon.aci.extenstions.pseudoPagination
 import com.github.leon.aci.extenstions.responseEntityOk
 import com.github.leon.aci.security.ApplicationProperties
-import com.github.leon.aci.service.GeneratorService
-import com.github.leon.aci.service.PermissionService
-import com.github.leon.aci.service.RoleService
 import com.github.leon.generator.entity.Task
-import com.github.leon.generator.entity.TaskOfProject
 import org.joor.Reflect
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.util.ClassUtils
 import org.springframework.web.bind.annotation.GetMapping
@@ -22,18 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/metadata")
-class MetadataController(
-        @Autowired
-        val userService: GeneratorService,
-        @Autowired
-        val permissionService: PermissionService,
-        @Autowired
-        val roleService: RoleService,
-        @Autowired
-        val permissionDao: PermissionDao,
-        @Autowired
-        val roleDao: RoleDao
-) {
+class MetadataController {
 
     fun findClasses(target: Class<*>, pattern: String): List<Class<*>> {
         val resourcePatternResolver = PathMatchingResourcePatternResolver()
@@ -48,24 +33,30 @@ class MetadataController(
     }
 
     @GetMapping("/entity")
-    fun entity(): ResponseEntity<List<Map<String, Any?>>> {
-        val list = ApplicationProperties.entityScanPackages.map { it.replace(".", "/") + "/*.class" }
+    fun entity(pageable: Pageable): ResponseEntity<Page<MutableMap<String, String>>> {
+        val allContent = ApplicationProperties.entityScanPackages.map { it.replace(".", "/") + "/*.class" }
                 .flatMap {
                     findClasses(BaseEntity::class.java, it)
                 }.map { mutableMapOf("name" to it.name) }
-        return ResponseEntity.ok(list)
+        return allContent.pseudoPagination(pageable).responseEntityOk()
 
     }
 
+
     @GetMapping("/task")
-    fun task(): ResponseEntity<List<Task>> {
-        Reflect.on("com.rapiddev.generator.TaskConstants").call("init")
+    fun task(pageable: Pageable): ResponseEntity<Page<Task>> {
+        //TaskConstants.init()
+        val fqn = "com.rapiddev.generator.TaskConstants"
+        val clz: Class<*> = Class.forName(fqn)
+        val instance = clz.kotlin.objectInstance
+        Reflect.on(instance).call("init")
         val list = findClasses(Task::class.java, "classpath*:com/rapiddev/generator/task/*/*.class")
                 .map {
                     (it.newInstance() as Task)
                 }
-        return list.responseEntityOk()
+        return list.pseudoPagination(pageable).responseEntityOk()
 
     }
 
 }
+
