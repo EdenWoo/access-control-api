e67442a526038a0bbad42054de3a460c1a23b69b
leonzhou <lei.zhou@cfgglobal.co.nz>
2018-06-19 16:02:53 +1200

    task

--- src/main/kotlin/com/github/leon/template/task/api/BaseControllerTask.kt
+++ src/main/kotlin/com/github/leon/template/task/api/BaseControllerTask.kt
@@ -3,7 +3,6 @@ package com.github.leon.template.task.api
 import com.github.leon.generator.entity.Task
 import com.github.leon.generator.entity.TaskOfProject
 import com.github.leon.template.TaskConstants
-import com.github.leon.template.excelProcessor
 
 class BaseControllerTask : Task(
         active = true,
@@ -13,5 +12,5 @@ class BaseControllerTask : Task(
         taskType = "multiple",
         filename = """ "Base"+entity.name+"Controller.kt" """,
         templatePath = """ "kotlin/baseController.ftl" """
-      //  entityExtProcessor = excelProcessor
+        //  entityExtProcessor = excelProcessor
 )
